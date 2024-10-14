ca58c539f7b6d5c03f6cd09848a30dfe35af26cd
faizanzahid107 <mrdevelopers087@gmail.com>
2024-10-14 22:51:07 +1100

    Unauthorised access blocked

--- admin/client/App/App.js
+++ admin/client/App/App.js
@@ -3,7 +3,7 @@
  * contains common things like navigation, footer, etc.
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Container } from './elemental';
 import { Link } from 'react-router';
 import { css } from 'glamor';
@@ -13,131 +13,257 @@ import MobileNavigation from './components/Navigation/Mobile';
 import PrimaryNavigation from './components/Navigation/Primary';
 import SecondaryNavigation from './components/Navigation/Secondary';
 import Footer from './components/Footer';
+import { rolePermissions } from '../constants';
 // import { Header } from './components/Header';
 
 import IframeContent from './shared/IframeContent';
 
 const classes = {
-	wrapper: {
-		display: 'flex',
-		flexDirection: 'column',
-		minHeight: '100vh',
-	},
-	header: {
-		position: "sticky", 
-		top: 0,
-		backgroundColor: "#1385e5",
-		color: "white",
-		zIndex: 1,
-		minHeight: '40px',
-	},
-	cover: {
-		objectFit: 'cover',
-		minWidth: '180px',
-		minHeight: '40px',
-	},
-	body: {
-		flexGrow: 1,
-	},
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  header: {
+    position: 'sticky',
+    top: 0,
+    backgroundColor: '#1385e5',
+    color: 'white',
+    zIndex: 1,
+    minHeight: '40px',
+  },
+  cover: {
+    objectFit: 'cover',
+    minWidth: '180px',
+    minHeight: '40px',
+  },
+  body: {
+    flexGrow: 1,
+  },
 };
 
-const App = (props) => {
-	const listsByPath = require('../utils/lists').listsByPath;
-	let children = props.children;
-	// If we're on either a list or an item view
-	let currentList, currentSection;
-	if (props.params.listId) {
-		currentList = listsByPath[props.params.listId];
-		// If we're on a list path that doesn't exist (e.g. /keystone/gibberishasfw34afsd) this will
-		// be undefined
-		if (!currentList) {
-			const section = _.find(Keystone.nav.sections, {lists: [{path: props.location.pathname, external: true}] })
-			if (section) {
-				const path = _.find(section.lists, {path: props.location.pathname, external: true});
-				console.log(path);
-				children = (<IframeContent src={path.href} show={true} onCancel={() => {
-					console.log('frame cancel');
-				}} onSave={() => {
-					console.log('frame save');
-				}} />)
-			} else {
-				children = (
-					<Container>
-						<p>List not found!</p>
-						<Link to={`${Keystone.adminPath}`}>
-							Go back home
-						</Link>
-					</Container>
-				);
-			}
-		} else {
-			// Get the current section we're in for the navigation
-			currentSection = Keystone.nav.by.list[currentList.key];
-		}
-	}
-	// Default current section key to dashboard
-	const currentSectionKey = (currentSection && currentSection.key) || 'dashboard';
-	return (
-		<div className={css(classes.wrapper)}>
-			<div className={css(classes.header)}>
-				<div style={{minWidth: '180px', float: 'left'}}>
-					<a href="/" title="Front page - Mojo Manager">
-						<img width="175" height="40" src="../../secure/images/header_logo.png"></img> 
-					</a>
-				</div>
-				<div style={{paddingLeft: '30px', float: 'left', fontFamily: "'Faster One', cursive", fontSize: '28px'}}>
-					{Keystone.user.companyName}
-				</div>
-				<div style={{float: 'right', paddingRight: '30px', paddingTop: '8px'}}>					
-					Signed in as, <b>{Keystone.user.name}</b>
-					<a href="/secure/signout" style={{paddingLeft: '16px', cursor: 'pointer', color: 'white'}}>Sign Out</a>  					
-				</div>
-				
-			</div>		
-			<header>				
-				<MobileNavigation
-					brand={Keystone.brand}
-					currentListKey={props.params.listId}
-					currentSectionKey={currentSectionKey}
-					sections={Keystone.nav.sections}
-					signoutUrl={Keystone.signoutUrl}
-				/>
-				{/* <Header
-					appversion={Keystone.appversion}
-					backUrl={Keystone.backUrl}
-					brand={Keystone.brand}
-					User={Keystone.User}
-					user={Keystone.user}
-					version={Keystone.version}
-				/> */}
-				<PrimaryNavigation
-					currentSectionKey={currentSectionKey}
-					brand={Keystone.brand}
-					sections={Keystone.nav.sections}
-					signoutUrl={Keystone.signoutUrl}
-				/>
-				{/* If a section is open currently, show the secondary nav */}
-				{(currentSection) ? (
-					<SecondaryNavigation
-						currentListKey={props.params.listId}
-						lists={currentSection.lists}
-						itemId={props.params.itemId}
-					/>
-				) : null}
-			</header>
-			<main className={css(classes.body)}>
-				{children}
-			</main>
-			<Footer
-				appversion={Keystone.appversion}
-				backUrl={Keystone.backUrl}
-				brand={Keystone.brand}
-				User={Keystone.User}
-				user={Keystone.user}
-				version={Keystone.version}
-			/>
-		</div>
-	);
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      userRoles: [],
+      isSuperAdmin: false,
+      loading: true, // To handle loading state
+    };
+    
+    this.isRouteAllowed = this.isRouteAllowed.bind(this);
+  }
+
+  componentDidMount() {
+    const apiHeaders = {
+      headers: {
+        'Cache-Control': 'no-cache',
+        Authorization: Keystone.user.token, // Ensure Keystone.user is defined and accessible
+      },
+    };
+
+    fetch(`${window.location.origin}/app/users/me`, apiHeaders)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((userDetails) => {
+        console.log('result-----userDetails:', userDetails);
+        const { user } = userDetails;
+        console.log('result-----USER:', user);
+
+        this.setState({
+          userRoles: user.rolesName,
+          loading: false,
+          isSuperAdmin: (user.role && user.role == "SuperAdmin") ? true : false
+        });
+      })
+      .catch((e) => {
+        console.error('USER ERR->>', e);
+        this.setState({ loading: false });
+        // Optionally, handle unauthorized access here
+      });
+  }
+
+  /**
+   * Helper function to determine if the current path is allowed for the user's roles
+   * @param {string} currentPath - The current route path
+   * @returns {boolean} - Returns true if the route is allowed, else false
+   */
+  isRouteAllowed(currentPath) {
+    const { userRoles, isSuperAdmin } = this.state;
+    console.log("isSuperAdmin", isSuperAdmin, "state:", this.state)
+    if(isSuperAdmin){
+      return true;
+    }
+    console.log("userRoles", userRoles)
+    if(userRoles && userRoles.length > 0){
+    for (let i = 0; i < userRoles.length; i++) {
+      const role = userRoles[i];
+      const allowedRoutes = rolePermissions[role] || [];
+    console.log("rolePermissions", rolePermissions)
+    console.log("allowedRoutes", allowedRoutes)
+
+      for (let j = 0; j < allowedRoutes.length; j++) {
+        const route = allowedRoutes[j];
+        // Exact match; modify if you need pattern matching
+        if (currentPath === route) {
+          return true;
+        }
+      }
+    }
+  }
+    return false;
+  }
+
+  render() {
+    const { userRoles, loading } = this.state;
+    const { children: propChildren, params, location } = this.props;
+
+    const listsByPath = require('../utils/lists').listsByPath;
+    let children = propChildren;
+
+    console.log('listsByPath', listsByPath, 'props', this.props, 'WINDOWS:');
+    console.log('Window:', window.location);
+    console.log('Keystone.user:', Keystone.user);
+    console.log('Keystone:', Keystone.user);
+
+    // if (loading) {
+    //   return <div>Loading...</div>;
+    // }
+
+    // If we're on either a list or an item view
+    let currentList, currentSection;
+    if (params.listId) {
+      currentList = listsByPath[params.listId];
+      const currentPath = location.pathname;
+
+      // If we're on a list path that doesn't exist (e.g. /keystone/gibberishasfw34afsd) this will be undefined
+      if (!currentList) {
+        const section = _.find(Keystone.nav.sections, {
+          lists: [{ path: currentPath, external: true }],
+        });
+        if (section) {
+          const path = _.find(section.lists, { path: currentPath, external: true });
+          console.log(path);
+          children = (
+            <IframeContent
+              src={path.href}
+              show={true}
+              onCancel={() => {
+                console.log('frame cancel');
+              }}
+              onSave={() => {
+                console.log('frame save');
+              }}
+            />
+          );
+        } else {
+          children = (
+            <Container>
+              <p>Page not found!</p>
+              <Link to={`${Keystone.adminPath}`}>Go back home</Link>
+            </Container>
+          );
+        }
+      } else {
+        console.log('ELSE1', !this.isRouteAllowed(params.listId));
+        if (!this.isRouteAllowed(params.listId)) {
+          children = (
+            <Container>
+              <p>Access blocked!</p>
+              <Link to={`${Keystone.adminPath}`}>Go back home</Link>
+            </Container>
+          );
+        } else {
+          // Get the current section we're in for the navigation
+          currentSection = Keystone.nav.by.list[currentList.key];
+        }
+      }
+    }
+
+    // Default current section key to dashboard
+    const currentSectionKey = (currentSection && currentSection.key) || 'dashboard';
+
+    return (
+      <div className={css(classes.wrapper)}>
+        <div className={css(classes.header)}>
+          <div style={{ minWidth: '180px', float: 'left' }}>
+            <a href="/" title="Front page - Mojo Manager">
+              <img
+                width="175"
+                height="40"
+                src="../../secure/images/header_logo.png"
+                alt="Logo"
+              />
+            </a>
+          </div>
+          <div
+            style={{
+              paddingLeft: '30px',
+              float: 'left',
+              fontFamily: "'Faster One', cursive",
+              fontSize: '28px',
+            }}
+          >
+            {Keystone.user.companyName}
+          </div>
+          <div style={{ float: 'right', paddingRight: '30px', paddingTop: '8px' }}>
+            Signed in as, <b>{Keystone.user.name}</b>
+            <a
+              href="/secure/signout"
+              style={{ paddingLeft: '16px', cursor: 'pointer', color: 'white' }}
+            >
+              Sign Out
+            </a>
+          </div>
+        </div>
+        <header>
+          <MobileNavigation
+            brand={Keystone.brand}
+            currentListKey={params.listId}
+            currentSectionKey={currentSectionKey}
+            sections={Keystone.nav.sections}
+            signoutUrl={Keystone.signoutUrl}
+          />
+          {/* <Header
+            appversion={Keystone.appversion}
+            backUrl={Keystone.backUrl}
+            brand={Keystone.brand}
+            User={Keystone.User}
+            user={Keystone.user}
+            version={Keystone.version}
+          /> */}
+          <PrimaryNavigation
+            currentSectionKey={currentSectionKey}
+            brand={Keystone.brand}
+            sections={Keystone.nav.sections}
+            signoutUrl={Keystone.signoutUrl}
+          />
+          {/* If a section is open currently, show the secondary nav */}
+          {currentSection ? (
+            <SecondaryNavigation
+              currentListKey={params.listId}
+              lists={currentSection.lists}
+              itemId={params.itemId}
+            />
+          ) : null}
+        </header>
+        <main className={css(classes.body)}>{children}</main>
+        <Footer
+          appversion={Keystone.appversion}
+          backUrl={Keystone.backUrl}
+          brand={Keystone.brand}
+          User={Keystone.User}
+          user={Keystone.user}
+          version={Keystone.version}
+        />
+      </div>
+    );
+  }
+}
 
 module.exports = App;
--- admin/client/constants.js
+++ admin/client/constants.js
@@ -41,3 +41,64 @@ exports.spacing = {
 
 exports.TABLE_CONTROL_COLUMN_WIDTH = 26;  // icon + padding
 exports.NETWORK_ERROR_RETRY_DELAY = 500; // in ms
+
+// routes
+export const rolePermissions = {
+	'User Management': ['users', 'sessions', 'companies', 'sub_companies', 'stores'],
+	'Order Management': [
+		'sale_ledgers',
+		'sale_ledger_items',
+		'sale_order_batches',
+		'sale_order_fee_batches',
+		'dispatch_order_batches',
+		'transfer_outs',
+		'adjust_outs',
+		'credit_note_outs',
+		'shipping_labels',
+		'previous_orders',
+		'customers',
+		'active_listings',
+		'list_items',
+		'list_variations',
+		'amazon_list_items',
+		'courier_companies',
+		'courier_rule_types',
+		'courier_services',
+		'shipment_rules',
+		'customer_delivery_addresses',
+		'customer_product_prices',
+		'customer_payments',
+	],
+	'Stock Management': [
+		'stock_in_ledgers',
+		'stock_out_ledgers',
+		'products',
+		'specs',
+		'suppliers',
+		'warehouses',
+		'product_suppliers',
+		'product_warehouses',
+		'product_groups',
+		'brands',
+		'categories',
+		'item_types',
+	],
+	'Purchase Management': [
+		'purchase_orders',
+		'purchase_credit_notes',
+		'purchase_replacemnet_ins',
+		'purchase_return_ins',
+		'adjust_ins',
+		'transfer_ins',
+	],
+	'Message Administrator': [
+		'messages',
+		'return_messages',
+		'case_messages',
+		'resolved_messages',
+		'waiting_messages',
+		'message_templates',
+	'message_conversations',
+'message_attachments'],
+	// Add more roles and their corresponding routes as needed
+  };
