2f7e41dcde473362d3ec41c433e8ec03f6771d8e
faizanzahid107 <mrdevelopers087@gmail.com>
2025-06-08 14:27:37 +1000

    reload fix

--- admin/client/App/App.js
+++ admin/client/App/App.js
@@ -121,9 +121,9 @@ class App extends Component {
   render() {
     const { userRoles, loading, canLogin, isSuperAdmin } = this.state;
     const { children: propChildren, params, location } = this.props;
-    if(!canLogin && !isSuperAdmin){
-      window.location.href ='/';
-    }
+    // if(!canLogin && !isSuperAdmin){
+    //   window.location.href ='/';
+    // }
     const listsByPath = require('../utils/lists').listsByPath;
     let children = propChildren;
     console.log("this.props", this.props, this.state)
